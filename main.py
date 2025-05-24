768f3083e32b7689e41958e419144e52598388ac
Fahlevi20 <rozakalfi@gmail.com>
2025-05-24 12:13:35 +0700

    Remove deprecated app.py code and add new API endpoints for image upload and review

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-
-
+from api.api import router
 from loguru import logger
 from router.middleware import setup_middleware
 
@@ -11,24 +10,6 @@ app = FastAPI()
 
 # Initialize DB connection on startup
 setup_middleware(app)
-conn = None
-
-@app.on_event("startup")
-def startup():
-    logger.info("initialized database connection")
-    global conn
-    conn = get_connection()
-    if conn is None:
-        logger.error("Failed to establish database connection")
-        raise RuntimeError("Failed to establish database connection")
-
-
-@app.on_event("shutdown")
-def shutdown():
-    global conn
-    if conn:
-        logger.info("closing database connection")
-        conn.close()
 
 
 app.include_router(router)
